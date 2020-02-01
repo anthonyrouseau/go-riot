6d5a3cc674558b80abcba2408d918a6ce81a0f56
Anthony Rouseau <anthonyrouseau@anthonys-mbp.lan>
2020-02-01 15:43:22 -0800

    constants for route keys

---
+++ client/route_keys.go
@@ -0,0 +1,45 @@
+package client
+
+type routeKey int
+
+//Routes constants are used as keys to map client methods to rate limiters
+const (
+	routeAllSummonerChampionMastery routeKey = iota
+	routeSummonerChampionMastery
+	routeSummonerChampionMasteryScore
+	routeChampionRotations
+	routeLolChallenger
+	routeLolSummonerLeagueEntries
+	routeLolGrandmaster
+	routeLolLeague
+	routeLolMaster
+	routeStatus
+	routeLolAccountMatches
+	routeLolMatchTimeline
+	routeActiveGame
+	routeFeaturedGames
+	routeSummonerByAccount
+	routeSummonerByName
+	routeSummonerByPUUID
+	routeSummonerBySummonerID
+	routeTftChallenger
+	routeTftSummonerLeagueEntries
+	routeTftGrandmaster
+	routeTftLeague
+	routeTftMaster
+	routeTftMatchIDs
+	routeTftMatch
+	routeTftSummonerByAccount
+	routeTftSummonerByName
+	routeTftSummonerByPUUID
+	routeTftSummonerBySummonerID
+	routeThirdPartyCode
+	routeTournamentMatchIDs
+	routeTournamentMatch
+	routeTournamentCodes
+	routeTournamentCodeInfo
+	routeUpdateTournamentCode
+	routeLobbyEvents
+	routeTournamentProvider
+	routeTournament
+)
