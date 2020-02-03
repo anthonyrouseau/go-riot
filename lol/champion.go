2b6ab138d76583afc1db0fb8e5e5545562886ec9
Anthony Rouseau <anthonyrouseau@anthonys-mbp.lan>
2020-02-02 18:11:13 -0800

    champion json tags

--- lol/champion.go
+++ lol/champion.go
@@ -7,27 +7,27 @@ type ChampionID int64
 
 //ChampionMastery represents a ChampionMasteryDTO containing mastery information for a single champion and summoner combination.
 type ChampionMastery struct {
-	ChestGranted         bool
-	Level                int32
-	Points               int32
-	ChampionID           ChampionID
-	PointsUntilNextLevel int64
-	LastPlayTime         int64
-	TokensEarned         int32
-	PointsSinceLastLevel int64
-	SummonerID           summoner.ID
+	ChestGranted         bool        `json:"chestGranted"`
+	Level                int32       `json:"championLevel"`
+	Points               int32       `json:"championPoints"`
+	ChampionID           ChampionID  `json:"championId"`
+	PointsUntilNextLevel int64       `json:"championPointsUntilNextLevel"`
+	LastPlayTime         int64       `json:"lastPlayTime"`
+	TokensEarned         int32       `json:"tokensEarned"`
+	PointsSinceLastLevel int64       `json:"championPointsSinceLastLevel"`
+	SummonerID           summoner.ID `json:"summonerId"`
 }
 
 //Rotation represents a ChampionInfo
 type Rotation struct {
-	FreeChampions           []ChampionID
-	FreeChampionsNewPlayers []ChampionID
-	MaxNewPlayerLevel       int32
+	FreeChampions           []ChampionID `json:"freeChampionIds"`
+	FreeChampionsNewPlayers []ChampionID `json:"freeChampionIdsForNewPlayers"`
+	MaxNewPlayerLevel       int32        `json:"maxNewPlayerLevel"`
 }
 
 //BannedChampion represents a BannedChampionDTO
 type BannedChampion struct {
-	PickTurn   int32
-	ChampionID ChampionID
-	TeamID     int64
+	PickTurn   int32      `json:"pickTurn"`
+	ChampionID ChampionID `json:"championId"`
+	TeamID     int64      `json:"teamId"`
 }
