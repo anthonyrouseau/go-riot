5421b3825119153098a3883691f1300d508c17ab
Anthony Rouseau <anthonyrouseau@anthonys-mbp.lan>
2020-02-02 18:05:47 -0800

    json tags for league structs

--- lol/league.go
+++ lol/league.go
@@ -16,50 +16,50 @@ type Division string
 
 //LeagueInfo represents a LeagueListDTO
 type LeagueInfo struct {
-	ID      LeagueID
-	Tier    Tier
-	Queue   queue.Name
-	Name    string
-	Entries []*LeagueItem
+	ID      LeagueID      `json:"leagueId"`
+	Tier    Tier          `json:"tier"`
+	Queue   queue.Name    `json:"queue"`
+	Name    string        `json:"name"`
+	Entries []*LeagueItem `json:"entries"`
 }
 
 //LeagueItem represents a LeagueItemDTO
 type LeagueItem struct {
-	SummonerName summoner.Name
-	HotStreak    bool
-	MiniSeries   *MiniSeries
-	Wins         int32
-	Veteran      bool
-	Losses       int32
-	FreshBlood   bool
-	Inactive     bool
-	Rank         string
-	SummonerID   summoner.ID
-	LeaguePoints int32
+	SummonerName summoner.Name `json:"summonerName"`
+	HotStreak    bool          `json:"hotStreak"`
+	MiniSeries   *MiniSeries   `json:"miniSeries"`
+	Wins         int32         `json:"wins"`
+	Veteran      bool          `json:"veteran"`
+	Losses       int32         `json:"losses"`
+	FreshBlood   bool          `json:"freshBlood"`
+	Inactive     bool          `json:"inactive"`
+	Rank         string        `json:"rank"`
+	SummonerID   summoner.ID   `json:"summonerId"`
+	LeaguePoints int32         `json:"leaguePoints"`
 }
 
 //LeagueEntry represents a LeagueEntryDTO
 type LeagueEntry struct {
-	QueueType    string
-	SummonerName summoner.Name
-	HotStreak    bool
-	MiniSeries   *MiniSeries
-	Wins         int32
-	Veteran      bool
-	Losses       int32
-	Rank         string
-	LeagueID     LeagueID
-	Inactive     bool
-	FreshBlood   bool
-	Tier         Tier
-	SummonerID   summoner.ID
-	LeaguePoints int32
+	QueueType    string        `json:"queueType"`
+	SummonerName summoner.Name `json:"summonerName"`
+	HotStreak    bool          `json:"hotStreak"`
+	MiniSeries   *MiniSeries   `json:"miniSeries"`
+	Wins         int32         `json:"wins"`
+	Veteran      bool          `json:"veteran"`
+	Losses       int32         `json:"losses"`
+	Rank         string        `json:"rank"`
+	LeagueID     LeagueID      `json:"leagueId"`
+	Inactive     bool          `json:"inactive"`
+	FreshBlood   bool          `json:"freshBlood"`
+	Tier         Tier          `json:"tier"`
+	SummonerID   summoner.ID   `json:"summonerId"`
+	LeaguePoints int32         `json:"leaguePoints"`
 }
 
 //MiniSeries represents a MiniSeriesDTO
 type MiniSeries struct {
-	Progress string
-	Losses   int32
-	Target   int32
-	Wins     int32
+	Progress string `json:"progress"`
+	Losses   int32  `json:"losses"`
+	Target   int32  `json:"target"`
+	Wins     int32  `json:"wins"`
 }
